e5f70402ae36fe39475fd9e2e9f87b7157d1add7
surajpatel9135 <patelsuraj62068@gmail.com>
2026-09-22 17:48:28 +0530

    simple logic of array traversal

---
+++ arrays/arrayTraversal/ArrayTraversal.java
@@ -0,0 +1,11 @@
+package arrays.arrayTraversal;
+
+public class ArrayTraversal {
+    public static void traverseArray(int[] arr, int n) {
+        System.out.println("Array elements are: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+    }
+}
