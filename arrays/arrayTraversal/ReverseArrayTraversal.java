0cbf13d977b85839c6a378baecb8f074f2b745f4
surajpatel9135 <patelsuraj62068@gmail.com>
2026-09-23 11:57:21 +0530

    logic of printing array in reverse or reverse traversal

--- arrays/arrayTraversal/ReverseArrayTraversal.java
+++ arrays/arrayTraversal/ReverseArrayTraversal.java
@@ -2,7 +2,7 @@ package arrays.arrayTraversal;
 
 public class ReverseArrayTraversal {
     public static void reverseArray(int[] arr) {
-        System.out.println("Array in reverse: ");
+        System.out.println("Print Array in reverse: ");
         for (int i = arr.length-1; i >= 0; i--) {
             System.out.print(arr[i]+ " ");
         }
